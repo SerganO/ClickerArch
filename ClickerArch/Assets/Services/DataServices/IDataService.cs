e7e638e58228b3bbbdad37407f1ad4c2492031b8
Serhii Ostrovetskyi <47448267+SerganO@users.noreply.github.com>
2021-02-16 19:25:31 +0200

    Добавил папки для сервисов Создан интерфейс IDataService Добавлена базовая реализация ResourcesDataService

---
+++ ClickerArch/Assets/Services/DataServices/IDataService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDataService
+{
+    AudioClip GetAudioClipForID(string id);
+    Sprite GetSpriteForID(string id); 
+}
---
+++ ClickerArch/Assets/Services/DataServices/ResourcesDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesDataService : IDataService
+{
+    public AudioClip GetAudioClipForID(string id)
+    {
+        return Resources.Load("AudioClips/" + id) as AudioClip;
+    }
+
+    public Sprite GetSpriteForID(string id)
+    {
+        return Resources.Load("Sprites/" + id) as Sprite;
+    }
+}
