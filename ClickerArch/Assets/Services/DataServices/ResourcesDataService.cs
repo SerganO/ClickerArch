20811f53f61218e5ef62ddef1da114a95b0ac2a7
Serhii Ostrovetskyi <47448267+SerganO@users.noreply.github.com>
2021-03-11 00:17:19 +0200

    Изменение Pivot в некоторых спрайтах врагов

--- ClickerArch/Assets/Services/DataServices/ResourcesDataService.cs
+++ ClickerArch/Assets/Services/DataServices/ResourcesDataService.cs
@@ -13,6 +13,10 @@ public class ResourcesDataService : IDataService
     {
         var result = new List<string>();
 
+        result.Add("Angel");
+        result.Add("Archer");
+        result.Add("BaldPirate");
+        result.Add("BigGuy");
         result.Add("inquisitor");
         result.Add("darkDoctor");
         result.Add("inquisitor");
